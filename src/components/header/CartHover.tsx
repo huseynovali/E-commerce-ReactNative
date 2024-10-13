cd6784fadecbd0da1e51a1cc23c14585844774c1
User <huseynovali612@gmail.com>
2024-10-13 14:53:19 +0400

    config

--- src/components/header/CartHover.tsx
+++ src/components/header/CartHover.tsx
@@ -11,60 +11,80 @@ function CartHover() {
   const dispatch = useDispatch();
   return (
     <View className=" bg-white p-2">
-      {cartProduct.length > 0 ?(cartProduct.map((item: any) => (
-        <View className="flex flex-row  items-center p-2" key={item.id}>
-          <View className="w-12 h-12 bg-gray-200 rounded-full">
-            <Image
-              source={{ uri: item.image }}
-              className="w-full h-full rounded-full object-cover"
-            />
-          </View>
+      {cartProduct.length > 0 ? (
+        cartProduct.map((item: any) => (
+          <View className="flex flex-row  items-center p-2" key={item.id}>
+            <View className="w-12 h-12 bg-gray-200 rounded-full">
+              <Image
+                source={{ uri: item.image }}
+                className="w-full h-full rounded-full object-cover"
+              />
+            </View>
 
-          <View className="flex flex-col ml-2 w-[200px]">
-            <Text>{item.title.substring(0, 20)}</Text>
-            <View className="flex flex-row items-center justify-between mt-1">
-              <Text>{item.price}$</Text>
-              <View className="flex flex-row items-center border border-gray-100 ml-3">
-                <TouchableOpacity
-                  onPress={() => {
-                    dispatch(ProductRemoveCard(item));
-                  }}
-                  className="bg-gray-400 w-[30px]"
-                >
-                  <Text
-                    style={{
-                      fontSize: 20,
-                      paddingHorizontal: 12,
-                      color: "white",
+            <View className="flex flex-col ml-2 w-[200px]">
+              <Text>{item.title.substring(0, 20)}</Text>
+              <View className="flex flex-row items-center justify-between mt-1">
+                <Text>{item.price}$</Text>
+                <View className="flex flex-row items-center border border-gray-100 ml-3">
+                  <TouchableOpacity
+                    onPress={() => {
+                      dispatch(ProductRemoveCard(item));
                     }}
+                    className="bg-gray-400 w-[30px]"
                   >
-                    -
+                    <Text
+                      style={{
+                        fontSize: 20,
+                        paddingHorizontal: 12,
+                        color: "white",
+                      }}
+                    >
+                      -
+                    </Text>
+                  </TouchableOpacity>
+                  <Text style={{ fontSize: 18, paddingHorizontal: 10 }}>
+                    {item.quantity}
                   </Text>
-                </TouchableOpacity>
-                <Text style={{ fontSize: 18, paddingHorizontal: 10 }}>
-                  {item.quantity}
-                </Text>
-                <TouchableOpacity
-                  onPress={() => {
-                    dispatch(ProductAddCart(item));
-                  }}
-                  className="bg-gray-500"
-                >
-                  <Text
-                    style={{
-                      fontSize: 20,
-                      paddingHorizontal: 10,
-                      color: "white",
+                  <TouchableOpacity
+                    onPress={() => {
+                      dispatch(ProductAddCart(item));
                     }}
+                    className="bg-gray-500"
                   >
-                    +
-                  </Text>
-                </TouchableOpacity>
+                    <Text
+                      style={{
+                        fontSize: 20,
+                        paddingHorizontal: 10,
+                        color: "white",
+                      }}
+                    >
+                      +
+                    </Text>
+                  </TouchableOpacity>
+                </View>
               </View>
             </View>
           </View>
+        ))
+      ) : (
+        <Text className="text-center">Cart is Empty</Text>
+      )}
+      {cartProduct.length > 0 && (
+        <View className="mt-2 flex flex-row w-full justify-between px-3">
+          <TouchableOpacity
+            onPress={() => {}}
+            className="bg-blue-500 p-2 rounded-full px-5"
+          >
+            <Text style={{ color: "white" }}>Go To Checkout</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="border border-blue-500 p-2 rounded-full px-5"
+          >
+            <Text style={{ color: "blue" }}>View Cart</Text>
+          </TouchableOpacity>
         </View>
-      ))): <Text className="text-center">Cart is Empty</Text>}
+      )}
     </View>
   );
 }
